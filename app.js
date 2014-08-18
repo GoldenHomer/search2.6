dac402478ada4ea533f85fae3f5f7c3081851d30
Jose Contreras <jocontre@joselcontreras.com>
2014-08-17 23:49:17 -0700

    More edits

--- app.js
+++ app.js
@@ -5,14 +5,10 @@ var mongodb = require('mongodb'),
 var assert = require('assert');
 var util = require('util');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
 
 var db; // Leave DB as global variable.
 
@@ -33,11 +29,11 @@ MongoClient.connect('mongodb://hosay:Snoogan9s@ds059509.mongolab.com:59509/26exp
     });
 });
 
-app.use('/', function(req, res){
+app.get('/', function(req, res){
     res.sendfile('./views/index.html');
 });
 
-app.use('/add', function(req, res){
+app.get('/add', function(req, res){
     res.sendfile('./views/add.html');
 });
 
@@ -90,37 +86,3 @@ function pageList(items){
     result = result + "</ul></body></html>";
     return result;
 }
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
